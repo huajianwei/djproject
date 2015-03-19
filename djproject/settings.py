27d366cf8e6279f63d702619481b047ee7a7ef3c
huajianwei2013@outlook.com <huajianwei2013@outlook.com>
2015-03-19 15:40:40 +0800

    Initial commit

--- djproject/settings.py
+++ djproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djproject.blog',
+    'djproject.blog'
 ]
 
 MIDDLEWARE_CLASSES = [
